43a3b9832aa4bcaf4ced09d905b7d6b8bd7409d9
Steve Yurong Su <rong@apache.org>
2023-01-23 15:54:46 +0800

    thrift

--- thrift-udf/src/main/thrift/udf.thrift
+++ thrift-udf/src/main/thrift/udf.thrift
@@ -19,3 +19,51 @@
 
 namespace java org.apache.iotdb.db.mpp.transformation.dag.udf.protocol.rpc
 namespace py iotdb.udf.protocol.rpc
+
+service UDFInvocationService {
+
+  BeforeStartResponse beforeStart(1:BeforeStartRequest request);
+
+  TransformResponse transformRows(1:TransformRowsRequest request);
+
+  TransformResponse transformWindows(1:TransformWindowsRequest request);
+}
+
+struct BeforeStartRequest {
+    1: required list<string> childExpressions
+    2: required list<string> childExpressionDataTypes
+    3: required map<string, string> attributes
+}
+
+struct BeforeStartResponse {
+    1: required string accessStrategyType
+    2: required map<string, string> accessStrategyAttributes
+    3: required string outputDataType
+}
+
+struct TransformResponse {
+    1: required binary timeColumn
+    2: required binary valueColumn
+}
+
+struct TransformRowsRequest {
+    1: required binary timeColumn
+    2: required list<binary> valueColumns
+    3: required list<binary> bitmaps
+    4: required bool isLastRequest
+}
+
+struct TransformWindowsRequest {
+    1: required binary timeColumn
+    2: required list<binary> valueColumns
+    3: required list<binary> bitmaps
+    4: required list<WindowMeta> windows
+    5: required bool isLastRequest
+}
+
+struct WindowMeta {
+    1: required i32 beginIndex
+    2: required i32 endIndex
+    3: required i32 beginTime
+    2: required i32 endTime
+}
